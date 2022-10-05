05cc9e46b168c3ae1c4f5fffc844b26a74a52d89
Samuel Newman <mozzius@protonmail.com>
2022-10-05 12:45:44 +0100

    use t3 stack

--- components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Layout } from "./Layout/Layout";
-export { Nav } from "./Nav/Nav";
-export { List } from "./List/List";
-export { CollectionDetailsList } from "./List/CollectionDetailsList";
-export { CollectionsList } from "./List/CollectionsList";
--- utils/hash.ts
+++
@@ -1,6 +0,0 @@
-import crypto from "crypto";
-
-const hash = (str: string) =>
-  crypto.createHash("sha256").update(str).digest("hex");
-
-export default hash;
